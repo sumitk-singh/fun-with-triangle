da52550b828415e29574976fdd6fdcecc53f9c98
sumit-0-0-1 <sumit.email.dev@gmail.com>
2021-11-22 21:01:07 +0530

    new page for angle of triangle has created

---
+++ src/quiz.js
@@ -0,0 +1,24 @@
+const quizForm = document.querySelector(".quiz-form-container");
+const submitAnswerBtn = document.querySelector("#submit-answer-btn");
+const output_of_quiz_form = document.querySelector("#quiz-score-output");
+
+const correctAnswers = ["90°", "right angled", "one right angle", "12, 16, 20", "Equilateral triangle", "85°", "10°", "a + b + c", "no", "45°"]
+
+function calculateScore() {
+
+    let score = 0;
+    let index = 0;
+    let formResults = new FormData(quizForm)
+
+    for (let value of formResults.values()) {
+        if (value === correctAnswers[index]) {
+            score = score + 1;
+
+        }
+        index = index + 1;
+    }
+    output_of_quiz_form.innerText = "Your score is " + score;
+
+}
+
+submitAnswerBtn.addEventListener("click", calculateScore);
